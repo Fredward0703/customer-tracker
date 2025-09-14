69d0d46a7fb5db402a7d776f9d6edc22b45a6593
Federico Montejo <fmontejo@usf.edu>
2025-09-14 17:15:56 -0400

    Added someone new and console.log'd it to have all 4 people appear

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,11 @@
 // Creating the customer and step 2
 let customers = [
-    {name:"Phil Foden",email:"PhilFoden47@email.com",purchases:["SoccerBall", "Shirt", "BallPump"]},
-    {name:"Erling Haaland",email:"ErlingHaaland9@email.com",purchases:["DjEquipment", "GoalNet", "Trophy"]},
+    {name:"Phil Foden",email:"PhilFoden47@email.com",purchases:["Soccer Ball", "Shirt", "Ball Pump"]},
+    {name:"Erling Haaland",email:"ErlingHaaland9@email.com",purchases:["Dj Equipment", "Goal Net", "Trophy"]},
     {name:"Bernardo Silva",email:"BernardoSilva20@email.com",purchases:["Flag", "Cleats", "Dog"]},
 ];
+
+
+//attempting step 3, adding someone new
+customers.push({name:"Ruben Dias",email:"RubenDias3@email.com",purchases:["Blue Moon", "Tooth Paste", "ArmBand"]});
 customers.forEach(obj => console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchases}`))
